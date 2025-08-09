9b97f1ff3064f9f81342769a5363174b4b01fbf7
noahtt7 <noahtaguma@gmail.com>
2025-08-09 14:47:35 +0900

    removed unique winners

--- src/main/java/com/example/KeibaStarter/Models/Race.java
+++ src/main/java/com/example/KeibaStarter/Models/Race.java
@@ -11,6 +11,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 
@@ -25,6 +27,7 @@ public class Race {
     private Set<Horse> racers;
 
     @OneToOne
+    @JoinColumn(name = "winner_name", referencedColumnName = "name")
     private Horse winner;
 
     public Race() {

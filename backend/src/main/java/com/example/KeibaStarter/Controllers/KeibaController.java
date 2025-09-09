feabc0deba0dc452d1ccf9988b3a2e51337e03ab
noahtt7 <noahtaguma@gmail.com>
2025-09-09 11:43:53 -0700

    Changed request mapping for keibacontroller to /keiba and added crossorigin

--- backend/src/main/java/com/example/KeibaStarter/Controllers/KeibaController.java
+++ backend/src/main/java/com/example/KeibaStarter/Controllers/KeibaController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.KeibaStarter.Models.Horse;
 import com.example.KeibaStarter.Service.HorseService;
 
+@CrossOrigin("http://localhost:5173/")
 @RestController
-@RequestMapping("/hi")
+@RequestMapping("/keiba")
 public class KeibaController {
 
     public final HorseService horseService;

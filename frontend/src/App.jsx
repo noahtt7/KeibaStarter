07182ebb08ad733bd16687f678106559c52ace62
noahtt7 <noahtaguma@gmail.com>
2025-12-02 09:07:34 -0800

    Removed not used imports in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,8 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { listRaces } from './services/KeibaService'
-import { createRace } from './services/KeibaService'
-import { Header } from './components/Header'
-import { RaceManager } from './components/RaceManager'
-import { RaceButton } from './components/RaceButton'
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from 'react-bootstrap/Nav'
-import NavBar from 'react-bootstrap/NavBar'
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useNavigate } from 'react-router-dom'
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import ChooseRacerPage from './ChooseRacerPage';
 import HomePage from './HomePage';
 

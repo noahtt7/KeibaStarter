c61da74579ff34a0f27856fd8971c4a407d8e06a
noahtt7 <noahtaguma@gmail.com>
2025-12-04 11:55:08 -0800

    Changed KeibaService.js to use https://keibabackend.onrender.com/ instead of localhost

--- frontend/src/services/KeibaService.js
+++ frontend/src/services/KeibaService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = 'http://localhost:8080/race/getwinner/4'
-const REST_API_SIM_RACE = 'http://localhost:8080/race/simulaterace/4'
-const REST_API_GET_WINNER = 'http://localhost:8080/race/getwinner/4'
-const REST_API_CREATE_RACE = 'http://localhost:8080/race/createrace/ak'
-const REST_API_GET_RACERS = 'http://localhost:8080/keiba/horses'
-const REST_API_GET_RECENT_RACEID = 'http://localhost:8080/race/count'
+const REST_API_BASE_URL = 'https://keibabackend.onrender.com//race/getwinner/4'
+const REST_API_SIM_RACE = 'https://keibabackend.onrender.com//race/simulaterace/4'
+const REST_API_GET_WINNER = 'https://keibabackend.onrender.com//race/getwinner/4'
+const REST_API_CREATE_RACE = 'https://keibabackend.onrender.com//race/createrace/ak'
+const REST_API_GET_RACERS = 'https://keibabackend.onrender.com//keiba/horses'
+const REST_API_GET_RECENT_RACEID = 'https://keibabackend.onrender.com//race/count'
 
 export const listRaces = () => axios.get(REST_API_BASE_URL);
 export const getWinner = () => axios.get(REST_API_GET_WINNER);

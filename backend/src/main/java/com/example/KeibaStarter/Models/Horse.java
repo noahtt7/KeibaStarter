7c1d1c3d63ef3700ac5516ef48b9b2fccd096ec0
noahtt7 <noahtaguma@gmail.com>
2025-12-10 11:00:56 -0800

    import

--- backend/src/main/java/com/example/KeibaStarter/Models/Horse.java
+++ backend/src/main/java/com/example/KeibaStarter/Models/Horse.java
@@ -3,7 +3,7 @@ package com.example.KeibaStarter.Models;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakart.persistence.Table;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;

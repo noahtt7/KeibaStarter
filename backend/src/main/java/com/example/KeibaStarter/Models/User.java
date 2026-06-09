ff17615c19d31244ca379d2900cff3470920b70a
noahtt7 <noahtaguma@gmail.com>
2026-06-09 11:35:28 -0700

    JMV login details and updated keibasheet.csv

---
+++ backend/src/main/java/com/example/KeibaStarter/Models/User.java
@@ -0,0 +1,64 @@
+import com.example.KeibaStarter.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.lang.annotation.Inherited;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Tablt(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails{
+    @Id 
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    private boolean enabled;
+    @Column(name = "verification_code")
+    private String verificationCode;
+    @Column(name = "verification_expiration")
+    private LocalDateTime verificaitonCodeExpiresAt;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
---
+++ backend/src/main/java/com/example/KeibaStarter/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+import com.example.KeibaStarter.Repository;
+
+import com.example.Models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
